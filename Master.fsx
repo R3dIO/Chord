1f79a9b32ebb65be6f7287d810dc2d34c7a74ae4
Anuj Koli <aanuj111@gmail.com>
2021-10-22 15:06:17 -0400

    Added find successor code

--- Master.fsx
+++ Master.fsx
@@ -10,8 +10,9 @@ open System.Collections.Generic
 //-------------------------------------- Initialization --------------------------------------//
 type ChordMessageTypes =
     | TotalNodes of int
-    | InitailizeRing of IActorRef []
-    | JoinRing of int
+    | JoinRing
+    | FindSuccessor of int
+    | InitializeRing
     | SetId of int
     | ConvergeRing
     | InitializeKeys of int
@@ -39,57 +40,58 @@ let nthroot n A =
     f (A / double n)
 //-------------------------------------- Utils --------------------------------------//
 
-//-------------------------------------- Master Actor --------------------------------------//
-let RingMaster(mailbox: Actor<_>) =
-    
-    let mutable nodeCount = 0
-    let mutable totalNumNodes = 0
-
-    let rec loop()= actor{
-        let! msg = mailbox.Receive();
-        match msg with 
-        | TotalNodes n -> totalNumNodes <- n
-        | InitailizeRing (numNodes) -> 
-            nodeCount <- nodeCount + 1
-            if nodeCount = totalNumNodes then
-                stopWatch.Stop()
-                printfn "Time for convergence: %f ms" stopWatch.Elapsed.TotalMilliseconds
-                printfn "------------- End Gossip -------------"
-                Environment.Exit(0)
-        | JoinRing nodeId ->
-            printfn "Node %i Requested to Join" nodeId
-        | ConvergeRing ->
-            nodeCount <- nodeCount + 1
-            if nodeCount = totalNumNodes then
-                stopWatch.Stop()
-                printfn "Time for convergence: %f ms" stopWatch.Elapsed.TotalMilliseconds
-                printfn "------------- End Transfer -------------"
-                Environment.Exit(0)
-        | _ -> ()
-
-        return! loop()
-    }            
-    loop()
-
-let master = spawn system "Master" RingMaster
-//-------------------------------------- Supervisor Actor --------------------------------------//
-
 //-------------------------------------- Worker Actor --------------------------------------//
     // Activate Gossip worker first initialize neighbour and then select a random node for Gossip
 
 let RingWorker (mailbox: Actor<_>) =
     let mutable nodeId = -1;
-    let mutable succesor = 0;
+    let mutable succesor = numNodes;
     let mutable predecessor = -1;
 
     let rec loop()= actor{
         let! message = mailbox.Receive();
+        match message with
+        | SetId Id ->
+            nodeId <- Id
+        | JoinRing ->
+            succesor <- succesorID
+        | _ -> ()
+        return! loop()
+    }            
+    loop()
+//-------------------------------------- Worker Actor --------------------------------------//
+
+//-------------------------------------- Master Actor --------------------------------------//
+let RingMaster(mailbox: Actor<_>) =
+    
+    let mutable requestCount = 0
+    let mutable totalNumNodes = 0
+
+    let rec loop()= actor{
+        let! msg = mailbox.Receive();
+        let response = mailbox.Sender();
         try
-            match message with
-            | SetId Id ->
-                nodeId <- Id
-            | JoinRing succesorID ->
-                succesor <- succesorID
+        match msg with 
+            | TotalNodes n -> totalNumNodes <- n
+            | InitializeRing ->
+                printfn "Intializing the ring"
+                let key = "RingWorker0" 
+                let worker = spawn system (key) RingWorker
+                worker <! SetId 0
+                nodeArray.Add(worker)
+                globalNodesDict.Add(0, worker)
+                worker <! JoinRing numNodes
+            | FindSuccessor nodeId ->
+                printfn "Node %i Requested to Join" nodeId
+                let successorId = [for id in nodeId .. numNodes do if globalNodesDict.ContainsKey id then yield id]
+                response <! successorId
+            | ConvergeRing ->
+                requestCount <- requestCount + 1
+                if requestCount = numRequests then
+                    stopWatch.Stop()
+                    printfn "Time for convergence: %f ms" stopWatch.Elapsed.TotalMilliseconds
+                    printfn "------------- End Transfer -------------"
+                    Environment.Exit(0)
             | _ -> ()
         with
             | :? System.IndexOutOfRangeException -> printfn "Tried to access outside array!" |> ignore
@@ -97,27 +99,30 @@ let RingWorker (mailbox: Actor<_>) =
     }            
     loop()
 
-
-//-------------------------------------- Worker Actor --------------------------------------//
+let master = spawn system "Master" RingMaster
+//-------------------------------------- Master Actor --------------------------------------//
 
 //-------------------------------------- Main Program --------------------------------------//
-for x in [0..numNodes] do
+stopWatch.Start()
+master <! TotalNodes(numNodes)
+master <! InitializeRing
+
+// Create nodes that will become part of ring
+for x in [1..numNodes] do
     let key: string = "RingWorker" + string(x)
     let worker = spawn system (key) RingWorker
     worker <! SetId x
     nodeArray.Add(worker)
     globalNodesDict.Add(x, worker)
 
-for x in [0..numNodes] do
+// Select a random node and join it to ring
+for x in [1..numNodes] do
     let rndNodeId = Random().Next(0, nodeArray.Count)
-    master <! JoinRing rndNodeId
+    let worker = globalNodesDict.[rndNodeId] 
+    let response =  (master <? FindSuccessor rndNodeId)
+    let successorId = Async.RunSynchronously (response, 2500)
+    worker <! JoinRing successorId 
     nodeArray.RemoveAt(rndNodeId) |> ignore
-
-stopWatch.Start()
-// Select a random worker to act as master
-let leader = Random().Next(0, numNodes)
-master <! TotalNodes(numNodes)
-
 
 Console.ReadLine() |> ignore
 //-------------------------------------- Main Program --------------------------------------//
